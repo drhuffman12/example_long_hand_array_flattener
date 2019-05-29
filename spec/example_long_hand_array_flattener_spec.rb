2269da03f384976aabd86dc541501afae67c491e
Daniel Huffman <drhuffman12@gmail.com>
2019-05-29 18:30:03 -0400

    config for CircleCI  and comment out forced failure

--- spec/example_long_hand_array_flattener_spec.rb
+++ spec/example_long_hand_array_flattener_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe ExampleLongHandArrayFlattener do
     expect(ExampleLongHandArrayFlattener::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
